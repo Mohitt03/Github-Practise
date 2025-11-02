40f6a34763e61b737b1796d7f2679340493e146f
Mohit Nagpure <mohitnapure@gmail.com>
2025-11-02 19:29:40 +0530

    This is second commit in Second Branch

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const Express = require('express')
 const app = Express()
 
 app.get('/', (req, res) => {
-    res.send('Hello')
+    res.send('This second branch')
 })
 
 app.listen(3000, () => { console.log("Server is running on 3000") })
